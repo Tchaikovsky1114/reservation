2e6305f083f8c01509a0b808eed248f199734a18
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-07-22 23:59:18 +0900

    fix: psql.js process env 수정

--- backend/psql.js
+++ backend/psql.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host:process.env.DB_HOST,
-  database:process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: 'postgres',
+  password:  process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 })
 
